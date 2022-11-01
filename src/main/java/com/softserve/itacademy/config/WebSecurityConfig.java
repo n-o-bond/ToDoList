7fb86c0dd2474fc8f68711d417927364400f8677
Natalia <n.o.bondarenko13@gmail.com>
2022-11-01 20:36:15 +0200

    added WebSecurityConfig

---
+++ src/main/java/com/softserve/itacademy/config/WebSecurityConfig.java
@@ -0,0 +1,75 @@
+package com.softserve.itacademy.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity(debug = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AccessDeniedHandler accessDeniedHandler(){
+        return new WebAccessDeniedHandler();
+    }
+
+    @Bean
+    public AuthenticationEntryPoint authenticationEntryPoint(){
+        return new WebAuthenticationEntryPoint();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers("/login-form", "/users/create").permitAll()
+                .antMatchers("/users/**").hasAuthority("ADMIN")
+                .antMatchers("/todos/**", "/").hasAnyAuthority("ADMIN", "USER")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login-form")
+                .defaultSuccessUrl("/")
+                .failureUrl("/login-form?error=true")
+                .permitAll()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/login-form")
+                .deleteCookies("JSESSIONID")
+                .permitAll();
+
+        http.exceptionHandling()
+                .authenticationEntryPoint(authenticationEntryPoint())
+                .accessDeniedHandler(accessDeniedHandler());
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(passwordEncoder())
+                .usersByUsernameQuery("select email, password, true from users where email=?;")
+                .authoritiesByUsernameQuery("select u.email, r.name from users u inner join roles r on u.role_id = r.id where u.email=?;");
+    }
+}
