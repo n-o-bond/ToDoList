bbd6f50bb427f36b255bb338f616266573a7b74b
Helen Sotnik <politovaoo@gamil.com>
2022-11-01 18:56:25 +0200

    Commit with UserServiceImp implement UserDetailsService

--- src/main/java/com/softserve/itacademy/service/impl/UserServiceImpl.java
+++ src/main/java/com/softserve/itacademy/service/impl/UserServiceImpl.java
@@ -4,14 +4,19 @@ import com.softserve.itacademy.exception.NullEntityReferenceException;
 import com.softserve.itacademy.model.User;
 import com.softserve.itacademy.repository.UserRepository;
 import com.softserve.itacademy.service.UserService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.springframework.security.core.userdetails.User.withUsername;
+
 @Service("userServiceImpl")
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserService, UserDetailsService {
 
     private UserRepository userRepository;
 
@@ -51,5 +56,18 @@ public class UserServiceImpl implements UserService {
     public List<User> getAll() {
         List<User> users = userRepository.findAll();
         return users.isEmpty() ? new ArrayList<>() : users;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        User user = userRepository.findByEmail(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found!");
+        }
+        org.springframework.security.core.userdetails.User.UserBuilder userBuilder = withUsername(user.getUsername());
+        userBuilder.password(user.getPassword());
+        userBuilder.roles(user.getRole().toString());
+
+        return userBuilder.build();
     }
 }
