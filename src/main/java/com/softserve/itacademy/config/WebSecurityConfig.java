f568db3d5a4c5b8e00866f2fa6071e26139eed40
Natalia <n.o.bondarenko13@gmail.com>
2022-11-01 20:51:59 +0200

    updated WebSecurityConfig

--- src/main/java/com/softserve/itacademy/config/WebSecurityConfig.java
+++ src/main/java/com/softserve/itacademy/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -43,14 +44,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf()
                 .disable()
                 .authorizeRequests()
-                .antMatchers("/login-form", "/users/create").permitAll()
+                .antMatchers(HttpMethod.GET,"/login-form").permitAll()
+                .antMatchers("/users/create").permitAll()
                 .antMatchers("/users/**").hasAuthority("ADMIN")
                 .antMatchers("/todos/**", "/").hasAnyAuthority("ADMIN", "USER")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
                 .loginPage("/login-form")
-                .defaultSuccessUrl("/")
+                .defaultSuccessUrl("/home")
                 .failureUrl("/login-form?error=true")
                 .permitAll()
                 .and()
