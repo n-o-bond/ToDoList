c8b87428a010f6d2e95ef374b5728e40b99295a7
Natalia <n.o.bondarenko13@gmail.com>
2022-11-02 23:14:03 +0200

    corrected accessDenied

--- src/main/java/com/softserve/itacademy/controller/LoginController.java
+++ src/main/java/com/softserve/itacademy/controller/LoginController.java
@@ -1,7 +1,12 @@
 package com.softserve.itacademy.controller;
 
+import com.softserve.itacademy.model.User;
+import com.softserve.itacademy.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.security.Principal;
 
 @Controller
 public class LoginController {
@@ -9,5 +14,15 @@ public class LoginController {
     @GetMapping("/login-form")
     public String login() {
         return "login-page";
+    }
+
+    @GetMapping("/accessDenied")
+    public ModelAndView accessDenied(Principal user) {
+        ModelAndView model = new ModelAndView();
+        if (user != null) {
+            model.addObject("error", "Sorry, you do not have permission to view this page!");
+        }
+        model.setViewName("accessDenied");
+        return model;
     }
 }
