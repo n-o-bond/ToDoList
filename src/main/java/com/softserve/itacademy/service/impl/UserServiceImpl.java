f9f56acd768f94afb2fde4a9dd2f225c02cb9116
Helen Sotnik <politovaoo@gamil.com>
2022-11-03 14:37:49 +0200

    Commit with header,login-page,home page,pom.xml final changes

--- src/main/java/com/softserve/itacademy/service/impl/UserServiceImpl.java
+++ src/main/java/com/softserve/itacademy/service/impl/UserServiceImpl.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
 import java.util.ArrayList;
+import java.util.List;
 
 @Service("userServiceImpl")
 public class UserServiceImpl implements UserService, UserDetailsService {
