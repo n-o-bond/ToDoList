a5c83d60dbae30c16ebf40b4b7b9492c6dd92d07
Oksana Lahutina <oksankalahutina@gmail.com>
2022-11-05 22:28:39 +0200

    Added JwtFilter

---
+++ src/main/java/com/softserve/itacademy/config/JwtFilter.java
@@ -0,0 +1,56 @@
+package com.softserve.itacademy.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@Component
+@Slf4j
+public class JwtFilter extends GenericFilterBean {
+
+    private static final String AUTHORIZATION_HEADER = "Authorization";
+    private static final String BEARER_PREFIX = "Bearer ";
+
+    private JwtProvider jwtProvider;
+    private CustomUserDetailsService customUserDetailsService;
+
+    @Autowired
+    public JwtFilter(JwtProvider jwtProvider, CustomUserDetailsService customUserDetailsService) {
+        this.jwtProvider = jwtProvider;
+        this.customUserDetailsService = customUserDetailsService;
+    }
+
+    private String getTokenFromRequest(HttpServletRequest request) {
+        String bearer = request.getHeader(AUTHORIZATION_HEADER);
+        if (bearer != null && bearer.startsWith(BEARER_PREFIX)) {
+            return bearer.substring(BEARER_PREFIX.length());
+        }
+        return null;
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+        log.info("**doFilter Start");
+        String token = getTokenFromRequest((HttpServletRequest) servletRequest);
+        if ((token != null) && jwtProvider.validateToken(token)) {
+            String login = jwtProvider.getLoginFromToken(token);
+            CustomUserDetails customUserDetails = customUserDetailsService.loadUserByUsername(login);
+            customUserDetails.setExpirationDate(jwtProvider.getExpirationDateFromToken(token));
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(customUserDetails, null, customUserDetails.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(servletRequest, servletResponse);
+        log.info("**doFilter End");
+    }
+}
